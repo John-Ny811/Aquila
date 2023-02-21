425e8e1e58f51fc3abbeb4eb88dd7f3dd7baa0f4
John-Ny811 <geo811811@gmail.com>
2023-02-21 14:17:05 +0100

    added header.php and footer.php

--- index.php
+++ index.php
@@ -12,18 +12,11 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1">
-
   <title>Wordpress-Theme</title>
-
-
-
-
-
-
-
 </head>
 
 <body>
-Hello World
+<header>Header</header>
+<footer>Footer</footer>
 </body>
 </html>
