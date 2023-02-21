4a692211566b15a7fd6a0f70e8cd23393722ea44
John-Ny811 <geo811811@gmail.com>
2023-02-21 13:45:52 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Main template get_included_file
+ *
+ * @package Aquila
+ */
+
+?>
+<!doctype html>
+
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1">
+
+  <title>Wordpress-Theme</title>
+
+
+
+
+
+
+
+</head>
+
+<body>
+Hello World
+</body>
+</html>
