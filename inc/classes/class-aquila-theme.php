22c07fccfa8e357c5ff8a15505325f547b59400c
John-Ny811 <geo811811@gmail.com>
2023-03-08 09:49:57 +0100

    finished tutorial #21

--- inc/classes/class-aquila-theme.php
+++ inc/classes/class-aquila-theme.php
@@ -48,6 +48,39 @@
 	      'default-image' => '',
         'default-repeat' => 'no-repeat',
       ] );
+
+      add_theme_support( 'post-thumbnails' );
+
+      add_theme_support( 'customize-selective-refresh-widgets' );
+
+      add_theme_support( 'automatic-feed-links' );
+
+      add_theme_support(
+        'html5',
+        [
+         'comment-list',
+         'comment-form',
+         'search-form',
+         'gallery',
+         'caption',
+         'style',
+         'script'
+        ]
+      );
+
+      add_editor_style();
+      add_theme_support( 'wp-block-styles' );
+      add_theme_support( 'align-wide' );
+
+      global $content_width;
+      if ( ! isset( $content_width ) ) {
+        $content_width = 1240;
+      }
+
+
+
   }
+
+
 
 }
