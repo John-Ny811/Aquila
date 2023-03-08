c73771d83bbd4c46063b27965db94c53326608ca
John-Ny811 <geo811811@gmail.com>
2023-03-08 09:19:24 +0100

    finished tutorial #20

--- inc/classes/class-aquila-theme.php
+++ inc/classes/class-aquila-theme.php
@@ -17,19 +17,37 @@
      //load class.
      Assets::get_instance();
      $this->setup_hooks();
-  }
+   }
 
-  protected function setup_hooks() {
+   protected function setup_hooks() {
 
-    /**
-     * Actions.
+      /**
+      * Actions.
      */
      add_action( 'after_setup_theme', [ $this, 'setup_theme'] );
-      add_theme_support( 'title-tag' );
-  }
 
-  public function setup_theme() {
 
+
+
+
+   }
+
+   public function setup_theme() {
+     add_theme_support( 'title-tag' );
+
+     add_theme_support( 'custom-logo', [
+        'header-text'          => [ 'site-title', 'site-description' ],
+        'height'               => 100,
+        'width'                => 400,
+        'flex-height'          => true,
+        'flex-width'           => true,
+      ] );
+
+      add_theme_support( 'custom-background', [
+        'default-color' => '#fff',
+	      'default-image' => '',
+        'default-repeat' => 'no-repeat',
+      ] );
   }
 
 }
